81b32e792ddfe8f6e048457e311251be15f32f2d
Laboratorio <mare.salazar790@gmail.com>
2026-05-14 14:58:57 -0500

    Primer commit

---
+++ Progra1historialcommits.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    int vector[7]= {3,6,9,12,15,18,21};
+    int *ptr;
+    ptr=&vector[0];
+    printf("%d", ptr);
+    for(int i=0; i<7; i++){
+        printf(" %d", vector[i]);
+    }
+    return 0;
+}
